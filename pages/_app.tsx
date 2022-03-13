e24b6e2f878545faa0d8f185ccf92c8720359796
Bernat Fogarasi <fogarasi.bernat@gmail.com>
2022-03-13 18:34:17 +0000

    Next.js and Firebase setup.

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import "config/firebase";
+
+import "../styles/globals.css";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
---
+++ pages/auth/index.tsx
@@ -0,0 +1,69 @@
+import { Button } from "@chakra-ui/react";
+import {
+  GoogleAuthProvider,
+  User,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { FaGoogle, FaSignOutAlt } from "react-icons/fa";
+import styled from "styled-components";
+
+const Wrapper = styled.div``;
+
+interface AuthProps {
+  className: string;
+}
+
+const Auth = ({ className }: AuthProps) => {
+  const [user, setUser] = useState<User | null>();
+
+  const onSignInClick = async () => {
+    const provider = new GoogleAuthProvider();
+    provider.setCustomParameters({
+      prompt: "select_account",
+    });
+    const auth = getAuth();
+    try {
+      const result = await signInWithPopup(auth, provider);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onSignOutClick = async () => {
+    const auth = getAuth();
+    try {
+      await signOut(auth);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user): void => {
+      setUser(user);
+    });
+  }, []);
+
+  return (
+    <Wrapper className={className}>
+      {user === undefined ? (
+        <>Loading...</>
+      ) : user ? (
+        <Button leftIcon={<FaSignOutAlt />} onClick={onSignOutClick}>
+          Sign out
+        </Button>
+      ) : (
+        <Button leftIcon={<FaGoogle />} onClick={onSignInClick}>
+          Sign in with Google
+        </Button>
+      )}
+    </Wrapper>
+  );
+};
+
+export default Auth;
