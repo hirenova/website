233b6cd2750fdf1642f9a197ed424464b92fb053
Bernat Fogarasi <fogarasi.bernat@gmail.com>
2022-04-09 17:06:51 +0100

    Corrected. Scrollable sidebar, freezing page when sidebar is open

--- hooks/usePage.tsx
+++
@@ -1,8 +0,0 @@
-import { PageContext } from "providers/PageProvider";
-import { useContext } from "react";
-
-const usePage = () => {
-  return useContext(PageContext);
-};
-
-export default usePage;
